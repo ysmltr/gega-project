524158ccbd540e2f87e0e0fc93186810c9096920
ysmltr <yesimltr1@gmail.com>
2023-10-26 16:00:11 +0300

    fonts and colors added

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily:{
+        gemunu:['Gemunu Libre', 'sans-serif'],
+        open:['Open Sans', 'sans-serif']
+      },
+      colors:{
+        'gega-red':'#EA4335',
+        'gega-melon': '#FFD369',
+        'gega-grey' : '#DDDDDD',
+        'gega-white' : '#F7F7F7'
+      }
+    },
   },
   plugins: [],
 }
